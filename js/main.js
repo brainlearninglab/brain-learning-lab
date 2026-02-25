0a231ea5cc9b2311fc2f6a9b8f9def936b246dcc
brainlearninglab <brain.learning.lab@gmail.com>
2026-02-25 14:11:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-import { installPageScale } from "./scale.js";
 import { initBrainPanelAutoRotate } from "./brain.js";
 import { initEEG } from "./eeg.js";
 import { initKid } from "./kid.js";
 import { initPanel } from "./panel.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  installPageScale({ safe: 80, topBias: 0, maxScale: 1 });
   initPanel();
   initBrainPanelAutoRotate();
   initKid();
